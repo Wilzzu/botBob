8326eebe21bbce8b60b2e2adb698f3e0c7903af4
Wille Karvinen <wille.karvinen@student.lab.fi>
2024-01-23 06:43:24 +0200

    Add rare messages

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,13 +2,17 @@ const { Events } = require("discord.js");
 const executeChatCommand = require("../utils/executeChatCommand");
 const duplicateMessage = require("../features/duplicateMessage");
 const respondToSpecificWord = require("../features/respondToSpecificWord");
-const sendCustomResponse = require("../features/sendCustomResponse");
+const customResponses = require("../features/customResponses");
 const { debtChannelID } = require("../configs/config.json");
+const rareMessages = require("../features/rareMessages");
 
 module.exports = {
 	name: Events.MessageCreate,
 	execute(msg) {
 		if (msg.author.bot) return;
+
+		// Delete new messages in debt channel
+		if (msg.channelId === debtChannelID && !msg.author.bot) return msg.delete();
 
 		// Respond to specific messages with the same message
 		if (duplicateMessage(msg)) return;
@@ -19,10 +23,10 @@ module.exports = {
 		// If message mentions the bot and contains a specific word, respond to it
 		if (respondToSpecificWord(msg)) return;
 
-		// Delete new messages in debt channel
-		if (msg.channelId === debtChannelID && !msg.author.bot) return msg.delete();
-
 		// Randomly send a customized message to a specific user
-		sendCustomResponse(msg);
+		customResponses(msg);
+
+		// Randomly send a rare message
+		rareMessages(msg);
 	},
 };
