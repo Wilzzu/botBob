0b0b85d522fbed980fb46a1437927e5d22188f29
Wilzzu <wilzzudev@gmail.com>
2025-07-27 08:29:05 +0300

    Add forum thread support to clips feature
    - If the clip channel ID is set to a forum channel, each user will have their own thread for their clips
    - Automatically update the thread name to the user's current guild name

--- features/clips/clips.js
+++ features/clips/clips.js
@@ -1,30 +1,91 @@
 const cron = require("node-cron");
+const config = require("../../configs/config.json");
 const {
+	guildID,
 	features: { clips },
-} = require("../../configs/config.json");
+} = config;
 const getClubHighlights = require("./getClubClips");
-const { userMention } = require("discord.js");
+const { userMention, ThreadAutoArchiveDuration } = require("discord.js");
+const fs = require("fs");
 
 let client;
+let clipUsers = clips.users;
+let updateConfigUsers = false;
+
+const clipContent = (user, clip) => {
+	const username = user ? userMention(user.discord) : clip.username;
+	return `## 🎬 ${username} - [${clip.desc}](${clip?.videoUrl || clip.clipPageUrl})`;
+};
+
+const handleForum = async (forum, user, clip) => {
+	const thread = forum.threads.cache.find((thread) => thread.id === user?.threadId);
+	const member = forum.guild.members.cache.get(user?.discord);
+	const username = member
+		? member.nickname || member.user.globalName || member.user.username
+		: clip.username;
+
+	// If thread already exists for the user, update the name if needed and send the clip
+	if (thread) {
+		if (thread.name !== username) await thread.setName(username);
+		await thread.send(clipContent(user, clip));
+		return;
+	}
+
+	// Create new thread if user doesn't have one yet and send the clip
+	const newThread = await forum.threads.create({
+		name: username,
+		autoArchiveDuration: ThreadAutoArchiveDuration.OneDay,
+		message: {
+			content: clipContent(user, clip),
+		},
+	});
+	if (!newThread) return;
+
+	// Update config user data
+	if (user) {
+		const clipUser = clipUsers.find((e) => e.steam === user.steam);
+		clipUser.threadId = newThread.id;
+	} else {
+		clipUsers.push({ name: clip.username, steam: clip.steamId, threadId: newThread.id });
+	}
+	updateConfigUsers = true;
+};
 
 const sendClipToChannel = async (clip) => {
-	const parseUser = clips.users.find((e) => e.steam === clip.steamId);
-	const user = parseUser ? userMention(parseUser.discord) : clip.username;
-	const message = `## 🎬 ${user} - [${clip.desc}](${clip?.videoUrl || clip.clipPageUrl})`;
-
 	const channel = client.channels.cache.get(clips.channelId);
 	if (!channel) return console.log("❌ Couldn't find the clip channel!");
 
-	await channel.send(message);
+	const user = clipUsers.find((e) => e.steam === clip.steamId);
+
+	// Send to forum thread or normal text channel
+	if (channel.type === 15) {
+		await handleForum(channel, user, clip);
+		return;
+	}
+	await channel.send(clipContent(user, clip));
 };
 
 // Check recent club highlights
 const checkForClubClips = async () => {
-	const clips = await getClubHighlights();
-	if (!clips) return;
+	const foundClips = await getClubHighlights();
+	if (!foundClips) return;
+
+	// Update guild user cache so we can get the latest nicknames
+	await client.guilds.cache.get(guildID)?.members.fetch();
 
 	// Send new clips to channel
-	clips.forEach((clip) => sendClipToChannel(clip));
+	for (const clip of foundClips) {
+		await sendClipToChannel(clip);
+	}
+
+	// Update config if needed
+	if (updateConfigUsers) {
+		updateConfigUsers = false;
+		clips.users = clipUsers;
+		fs.writeFile("./configs/config.json", JSON.stringify(config, null, 4), (err) => {
+			if (err) console.log(err);
+		});
+	}
 };
 
 const startClips = (bot) => {
